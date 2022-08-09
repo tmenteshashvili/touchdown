e4ad8cfa401f88a7cc73bd1b71899e7536813e66
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2022-08-09 12:19:19 +0400

    Initial Commit

---
+++ Touchdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Touchdown
+//
+//  Created by Tako Menteshashvili on 09.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Tako Menteshashvili on 09.08.22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
